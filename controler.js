9bba2eab61f0adb0bfe54fd3454e3bbac772f7d4
RisteRiki <102678685+RisteRiki@users.noreply.github.com>
2022-03-31 14:25:55 +0200

    Add files via upload

---
+++ controler.js
@@ -0,0 +1,22 @@
+// let xml = new XMLHttpRequest();
+
+// xml.open('GET', "https://raw.githubusercontent.com/RisteRiki/org-chart/master/misc/data.json");
+// xml.onreadystatechange = function () {
+//     if (xml.readyState === 4 && xml.status === 200) {
+//         displayData(JSON.parse(xml.responseText));
+//     }
+   
+
+// }
+
+// function displayData(data) {
+//     let text = "";
+// data.forEach(element => {
+//     text+= `
+    
+//     `
+// });
+
+  
+// }
+// xml.send();
